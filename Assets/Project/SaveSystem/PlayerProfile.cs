85906421247e120742c1b3bba0991a94eb82d668
tomi <herceg133@gmail.com>
2024-05-04 10:44:32 +0200

    Added Basic SaveSystem + Fixed MainMenu Portal Lag

---
+++ Assets/Project/SaveSystem/PlayerProfile.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace tomi.SaveSystem
+{
+    [System.Serializable]
+    public class PlayerProfile
+    {
+        public int masterVolume;
+        public int musicVolume;
+        public int effectsVolume;
+    }
+}
