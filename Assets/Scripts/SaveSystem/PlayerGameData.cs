d0b432e3f1cfa3b334ee939e70b736ca62c3288b
tomi <herceg133@gmail.com>
2024-05-09 21:55:27 +0200

    Added Unlocked Levels List to PlayerGameData

--- Assets/Scripts/SaveSystem/PlayerGameData.cs
+++ Assets/Scripts/SaveSystem/PlayerGameData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace tomi.SaveSystem
@@ -7,6 +8,13 @@ namespace tomi.SaveSystem
     [System.Serializable]
     public class PlayerGameData
     {
+        public List<string> unlockedLevels = new List<string>();
         public string currentLevelName;
+
+        public PlayerGameData()
+        {
+            unlockedLevels = new List<string>();
+            unlockedLevels.Add("Level0");
+        }
     }
 }
