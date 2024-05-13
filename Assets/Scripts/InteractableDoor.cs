5bb1a61590d2f041e204ce0bc2ed3cdd2eb8fb35
tomi <herceg133@gmail.com>
2024-05-13 14:38:42 +0200

    Added Interactable Door | Updated Level1

---
+++ Assets/Scripts/InteractableDoor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableDoor : InteractableObject
+{
+
+    [SerializeField] private Vector3 closedDoorRotation;
+    [SerializeField] private Vector3 openDoorRotation;
+    [SerializeField] private bool isDoorOpen = false;
+    [SerializeField] private bool interactOnce = false;
+    [SerializeField] private float speedMultiplier = 1.0f;
+
+    public override void Interact(PlayerReferences playerRef)
+    {
+        if (!CanInteract) return;
+
+        base.Interact(playerRef);
+
+        StopAllCoroutines();
+        StartCoroutine(AdjustDoorRotation());
+    }
+
+    private IEnumerator AdjustDoorRotation()
+    {
+        float time = 0;
+        float duration = 1.0f;
+        Vector3 startRotation = transform.localEulerAngles;
+        Vector3 endRotation;
+
+        if (isDoorOpen)
+        {
+            endRotation = closedDoorRotation;
+        }
+        else
+        {
+            endRotation = openDoorRotation;
+        }
+
+        while (time < duration)
+        {
+            transform.localEulerAngles = Vector3.Lerp(startRotation, endRotation, (time / duration) * speedMultiplier);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localEulerAngles = endRotation;
+        isDoorOpen = !isDoorOpen;
+        if (interactOnce)
+        {
+            CanInteract = false;
+            ShowOutlineValue = false;
+            message = "";
+        }
+    }
+}
